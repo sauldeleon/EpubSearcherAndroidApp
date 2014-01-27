1cab282cf50d2ca885952b2d0557435782241fff
Tirias <tirias666@gmail.com>
2014-01-28 00:02:10 +0100

    setting login configuration

--- EpubSearcherAndroidApp/src/com/epubsearcherandroidapp/MainActivity.java
+++ EpubSearcherAndroidApp/src/com/epubsearcherandroidapp/MainActivity.java
@@ -1,32 +1,146 @@
 package com.epubsearcherandroidapp;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+//import com.dropbox.android.sample.DBRoulette;
+//import com.dropbox.android.sample.R;
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
+import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session.AccessType;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "Dropbox";
 	
+	//Dropbox
 	final static private String APP_KEY = "4lux9y10nib629k";
 	final static private String APP_SECRET = "bub07yv0jzjqg4d";
-	final static private AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
+	final static private AccessType ACCESS_TYPE = AccessType.DROPBOX;
+	
+	// You don't need to change these, leave them alone.
+    final static private String ACCOUNT_PREFS_NAME = "prefs";
+    final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
+    final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
 
 	private DropboxAPI<AndroidAuthSession> mDBApi;
+	
+	private Button loginButton;
+	private boolean logged;
 
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//		
+//		// And later in some initialization function:
+//		AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+//		AndroidAuthSession session = new AndroidAuthSession(appKeys, ACCESS_TYPE);
+//		this.mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+//	}
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// In the class declaration section:
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// And later in some initialization function:
-		AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
-		AndroidAuthSession session = new AndroidAuthSession(appKeys, ACCESS_TYPE);
-		mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+        // We create a new AuthSession so that we can use the Dropbox API.
+        AndroidAuthSession session = buildSession();
+        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+
+        // Basic Android widgets
+        setContentView(R.layout.activity_main);
+
+        checkAppKeySetup();
+
+        loginButton = (Button)findViewById(R.id.buttonLoggin);
+
+        loginButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // This logs you out if you're logged in, or vice versa
+                if (logged) {
+                    logOut();
+                } else {
+                    // Start the remote authentication
+                    mDBApi.getSession().startAuthentication(MainActivity.this);
+                }
+            }
+        });
+	}
+        
+
+        //mDisplay = (LinearLayout)findViewById(R.id.logged_in_display);
+
+        // This is where a photo is displayed
+       // mImage = (ImageView)findViewById(R.id.image_view);
+
+        // This is the button to take a photo
+       // mPhoto = (Button)findViewById(R.id.photo_button);
+
+        /*mPhoto.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                // Picture from camera
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                // This is not the right way to do this, but for some reason, having
+                // it store it in
+                // MediaStore.Images.Media.EXTERNAL_CONTENT_URI isn't working right.
+
+                Date date = new Date();
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd-kk-mm-ss");
+
+                String newPicFile = df.format(date) + ".jpg";
+                String outPath = "/sdcard/" + newPicFile;
+                File outFile = new File(outPath);
+
+                mCameraFileName = outFile.toString();
+                Uri outuri = Uri.fromFile(outFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+                Log.i(TAG, "Importing New Picture: " + mCameraFileName);
+                try {
+                    startActivityForResult(intent, NEW_PICTURE);
+                } catch (ActivityNotFoundException e) {
+                    showToast("There doesn't seem to be a camera.");
+                }
+            }
+        });*/
+	
+	protected void onResume() {
+	    super.onResume();
+
+	    if (mDBApi.getSession().authenticationSuccessful()) {
+	        try {
+	            // Required to complete auth, sets the access token on the session
+	            mDBApi.getSession().finishAuthentication();
+
+	            AccessTokenPair tokens = mDBApi.getSession().getAccessTokenPair();
+	        } catch (IllegalStateException e) {
+	            Log.i("DbAuthLog", "Error authenticating", e);
+	        }
+	    }
 	}
 
 	@Override
@@ -35,5 +149,102 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true; 
 	}
+	
+	private AndroidAuthSession buildSession() {
+        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session;
+
+        String[] stored = getKeys();
+        if (stored != null) {
+            AccessTokenPair accessToken = new AccessTokenPair(stored[0], stored[1]);
+            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE, accessToken);
+        } else {
+            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
+        }
+
+        return session;
+    }
+	
+	 /**
+     * Shows keeping the access keys returned from Trusted Authenticator in a local
+     * store, rather than storing user name & password, and re-authenticating each
+     * time (which is not to be done, ever).
+     *
+     * @return Array of [access_key, access_secret], or null if none stored
+     */
+    private String[] getKeys() {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        String key = prefs.getString(ACCESS_KEY_NAME, null);
+        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+        if (key != null && secret != null) {
+        	String[] ret = new String[2];
+        	ret[0] = key;
+        	ret[1] = secret;
+        	return ret;
+        } else {
+        	return null;
+        }
+    }
+    
+    private void checkAppKeySetup() {
+        // Check to make sure that we have a valid app key
+        if (APP_KEY.startsWith("CHANGE") ||
+                APP_SECRET.startsWith("CHANGE")) {
+            showToast("You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it.");
+            finish();
+            return;
+        }
+
+        // Check if the app has set up its manifest properly.
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        String scheme = "db-" + APP_KEY;
+        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
+        testIntent.setData(Uri.parse(uri));
+        PackageManager pm = getPackageManager();
+        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
+            showToast("URL scheme in your app's " +
+                    "manifest is not set up correctly. You should have a " +
+                    "com.dropbox.client2.android.AuthActivity with the " +
+                    "scheme: " + scheme);
+            finish();
+        }
+    }
+    
+    private void showToast(String msg) {
+        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        error.show();
+    }
+    
+    private void logOut() {
+        // Remove credentials from the session
+        mDBApi.getSession().unlink();
+
+        // Clear our stored keys
+        clearKeys();
+        // Change UI state to display logged out version
+        setLoggedIn(false);
+    }
+    
+    private void clearKeys() {
+        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+        Editor edit = prefs.edit();
+        edit.clear();
+        edit.commit();
+    }
+    
+    /**
+     * Convenience function to change UI state based on being logged in
+     */
+    private void setLoggedIn(boolean loggedIn) {
+    	logged = loggedIn;
+    	if (loggedIn) {
+    		loginButton.setText("Unlink from Dropbox");
+            //mDisplay.setVisibility(View.VISIBLE);
+    	} else {
+    		loginButton.setText("Link with Dropbox");
+           // mDisplay.setVisibility(View.GONE);
+           // mImage.setImageDrawable(null);
+    	}
+    }
 
 }
