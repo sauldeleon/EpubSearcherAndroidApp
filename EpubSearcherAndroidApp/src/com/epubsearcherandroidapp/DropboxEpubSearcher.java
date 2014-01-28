112ac61327fc4b2d6382ce73df6a5370e2ca38f7
Tirias <tirias666@gmail.com>
2014-01-28 01:03:41 +0100

    retrieving folder list

--- EpubSearcherAndroidApp/src/com/epubsearcherandroidapp/DropboxEpubSearcher.java
+++ EpubSearcherAndroidApp/src/com/epubsearcherandroidapp/DropboxEpubSearcher.java
@@ -1,5 +1,8 @@
 package com.epubsearcherandroidapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,172 +20,193 @@ import android.widget.Toast;
 //import com.dropbox.android.sample.DBRoulette;
 //import com.dropbox.android.sample.R;
 import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.DropboxAPI.Entry;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.android.AuthActivity;
 import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.TokenPair;
 import com.dropbox.client2.session.Session.AccessType;
 
 public class DropboxEpubSearcher extends Activity {
 	private static final String TAG = "Dropbox";
-	
-	//Dropbox
+
+	// Dropbox
 	final static private String APP_KEY = "4lux9y10nib629k";
 	final static private String APP_SECRET = "bub07yv0jzjqg4d";
 	final static private AccessType ACCESS_TYPE = AccessType.DROPBOX;
-	
+
 	// You don't need to change these, leave them alone.
-    final static private String ACCOUNT_PREFS_NAME = "prefs";
-    final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
-    final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
+	final static private String ACCOUNT_PREFS_NAME = "prefs";
+	final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
+	final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
 
 	private DropboxAPI<AndroidAuthSession> mDBApi;
-	
+
 	private Button loginButton;
 	private boolean logged;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        AndroidAuthSession session = buildSession();
-        mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+		AndroidAuthSession session = buildSession();
+		mDBApi = new DropboxAPI<AndroidAuthSession>(session);
 
-        // Basic Android widgets
-        setContentView(R.layout.activity_main);
+		// Basic Android widgets
+		setContentView(R.layout.activity_main);
 
-        checkAppKeySetup();
+		loginButton = (Button) findViewById(R.id.buttonLoggin);
 
-        loginButton = (Button)findViewById(R.id.buttonLoggin);
-
-        loginButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                 if (logged) {
-                    logOut();
-                } else {
-                    mDBApi.getSession().startAuthentication(DropboxEpubSearcher.this);
-                }
-            }
-        });
+		loginButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (logged) {
+					logOut();
+				} else {
+					mDBApi.getSession().startAuthentication(
+							DropboxEpubSearcher.this);
+				}
+			}
+		});
 	}
-	
+
+	@Override
 	protected void onResume() {
-	    super.onResume();
+		super.onResume();
+		AndroidAuthSession session = mDBApi.getSession();
 
-	    if (mDBApi.getSession().authenticationSuccessful()) {
-	        try {
-	            // Required to complete auth, sets the access token on the session
-	            mDBApi.getSession().finishAuthentication();
+		// The next part must be inserted in the onResume() method of the
+		// activity from which session.startAuthentication() was called, so
+		// that Dropbox authentication completes properly.
+		if (session.authenticationSuccessful()) {
+			try {
+				// Mandatory call to complete the auth
+				session.finishAuthentication();
 
-	            AccessTokenPair tokens = mDBApi.getSession().getAccessTokenPair();
-	        } catch (IllegalStateException e) {
-	            Log.i("DbAuthLog", "Error authenticating", e);
-	        }
-	    }
+				// Store it locally in our app for later use
+				TokenPair tokens = session.getAccessTokenPair();
+				storeKeys(tokens.key, tokens.secret);
+				setLoggedIn(true);
+			} catch (IllegalStateException e) {
+				showToast("Couldn't authenticate with Dropbox:"
+						+ e.getLocalizedMessage());
+				Log.i(TAG, "Error authenticating", e);
+			}
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true; 
+		return true;
 	}
-	
+
 	private AndroidAuthSession buildSession() {
-        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
-        AndroidAuthSession session;
+		AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+		AndroidAuthSession session;
 
-        String[] stored = getKeys();
-        if (stored != null) {
-            AccessTokenPair accessToken = new AccessTokenPair(stored[0], stored[1]);
-            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE, accessToken);
-        } else {
-            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
-        }
+		String[] stored = getKeys();
+		if (stored != null) {
+			AccessTokenPair accessToken = new AccessTokenPair(stored[0],
+					stored[1]);
+			session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE,
+					accessToken);
+		} else {
+			session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
+		}
 
-        return session;
-    }
-	
-	 /**
-     * Shows keeping the access keys returned from Trusted Authenticator in a local
-     * store, rather than storing user name & password, and re-authenticating each
-     * time (which is not to be done, ever).
-     *
-     * @return Array of [access_key, access_secret], or null if none stored
-     */
-    private String[] getKeys() {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        String key = prefs.getString(ACCESS_KEY_NAME, null);
-        String secret = prefs.getString(ACCESS_SECRET_NAME, null);
-        if (key != null && secret != null) {
-        	String[] ret = new String[2];
-        	ret[0] = key;
-        	ret[1] = secret;
-        	return ret;
-        } else {
-        	return null;
-        }
-    }
-    
-    private void checkAppKeySetup() {
-        // Check to make sure that we have a valid app key
-        if (APP_KEY.startsWith("CHANGE") ||
-                APP_SECRET.startsWith("CHANGE")) {
-            showToast("You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it.");
-            finish();
-            return;
-        }
+		return session;
+	}
 
-        // Check if the app has set up its manifest properly.
-        Intent testIntent = new Intent(Intent.ACTION_VIEW);
-        String scheme = "db-" + APP_KEY;
-        String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
-        testIntent.setData(Uri.parse(uri));
-        PackageManager pm = getPackageManager();
-        if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
-            showToast("URL scheme in your app's " +
-                    "manifest is not set up correctly. You should have a " +
-                    "com.dropbox.client2.android.AuthActivity with the " +
-                    "scheme: " + scheme);
-            finish();
-        }
-    }
-    
-    private void showToast(String msg) {
-        Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
-        error.show();
-    }
-    
-    private void logOut() {
-        // Remove credentials from the session
-        mDBApi.getSession().unlink();
+	/**
+	 * Shows keeping the access keys returned from Trusted Authenticator in a
+	 * local store, rather than storing user name & password, and
+	 * re-authenticating each time (which is not to be done, ever).
+	 * 
+	 * @return Array of [access_key, access_secret], or null if none stored
+	 */
+	private String[] getKeys() {
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		String key = prefs.getString(ACCESS_KEY_NAME, null);
+		String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+		if (key != null && secret != null) {
+			String[] ret = new String[2];
+			ret[0] = key;
+			ret[1] = secret;
+			return ret;
+		} else {
+			return null;
+		}
+	}
 
-        // Clear our stored keys
-        clearKeys();
-        // Change UI state to display logged out version
-        setLoggedIn(false);
-    }
-    
-    private void clearKeys() {
-        SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
-        Editor edit = prefs.edit();
-        edit.clear();
-        edit.commit();
-    }
-    
-    /**
-     * Convenience function to change UI state based on being logged in
-     */
-    private void setLoggedIn(boolean loggedIn) {
-    	logged = loggedIn;
-    	if (loggedIn) {
-    		loginButton.setText("Unlink from Dropbox");
-            //mDisplay.setVisibility(View.VISIBLE);
-    	} else {
-    		loginButton.setText("Link with Dropbox");
-           // mDisplay.setVisibility(View.GONE);
-           // mImage.setImageDrawable(null);
-    	}
-    }
 
+	private void showToast(String msg) {
+		Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+		error.show();
+	}
+
+	private void logOut() {
+		// Remove credentials from the session
+		mDBApi.getSession().unlink();
+
+		// Clear our stored keys
+		clearKeys();
+		// Change UI state to display logged out version
+		setLoggedIn(false);
+	}
+
+	private void clearKeys() {
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		Editor edit = prefs.edit();
+		edit.clear();
+		edit.commit();
+	}
+
+	/**
+	 * Convenience function to change UI state based on being logged in
+	 */
+	private void setLoggedIn(boolean loggedIn) {
+		logged = loggedIn;
+		if (loggedIn) {
+			loginButton.setText("Unlink from Dropbox");
+			// mDisplay.setVisibility(View.VISIBLE);
+		} else {
+			loginButton.setText("Link with Dropbox");
+			// mDisplay.setVisibility(View.GONE);
+			// mImage.setImageDrawable(null);
+		}
+	}
+
+	private void storeKeys(String key, String secret) {
+		// Save the access key for later
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		Editor edit = prefs.edit();
+		edit.putString(ACCESS_KEY_NAME, key);
+		edit.putString(ACCESS_SECRET_NAME, secret);
+		edit.commit();
+	}
+
+	private ArrayList<String> retrieveFolderList() {
+		ArrayList<String> folderName = new ArrayList<String>();
+		try {
+			Entry mainDropBoxDir = mDBApi.metadata("/", 0, null, true, null);
+			if (mainDropBoxDir.isDir) {
+				List<Entry> contents1 = mainDropBoxDir.contents;
+				if (contents1 != null) {
+					folderName.clear();
+					for (int i = 0; i < contents1.size(); i++) {
+						Entry e = contents1.get(i);
+						String a = e.fileName();
+						if (String.valueOf(e.isDir).equalsIgnoreCase("true")) {
+							folderName.add(a);
+							System.out.println(a.toString());
+						}
+					}
+				}
+			}
+		} catch (Exception ex) {
+		}
+		return folderName;
+	}
 }
